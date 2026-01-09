19d2ffbd49c686394d99cf4bd7be40528a1f2a12
Peter John Kimatarle <peterjohnkimatarle@gmail.com>
2026-01-09 15:15:26 +0300

    Clean up deleted file references from rentapp-v02 and rentapp-v05

--- Desktop/rentapp-v05/src/app/auth-ui-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthUIExamples from '@/components/AuthUIExamples'
-
-export default function AuthUIDemoPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-12">
-      <AuthUIExamples />
-    </div>
-  )
-}
